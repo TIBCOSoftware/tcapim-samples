ea5e3d61017d60098593ce3f17870e6f1c7860de
Mayur Kurkure <mkurkure@tibco.com>
2024-03-19 18:48:22 +0530

    ML 6.x Observability documentation

--- api-analytics/elastic_stack/swarm/teardown.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Copyright © 2021. TIBCO Software Inc.
-# This file is subject to the license terms contained
-# in the license file that is distributed with this file.
-#
-docker stack rm elk
-
